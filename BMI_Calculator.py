a2144cc22c77eac47f81a8173b5c22c42cc9f313
Justin Bardeloza <heartsniper0403@gmail.com>
2020-12-29 17:46:22 +0000

    Exercise7.1

---
+++ BMI_Calculator.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+H = float(height)
+W = float(weight)
+
+print (round(W / (H**2)))
